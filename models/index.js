d97c9a97d1140a7ccdfb46c93e495f499c5a16ac
Tmeng <tonymengt@gmail.com>
2022-09-27 07:55:53 -0400

    enabled category routes

--- models/index.js
+++ models/index.js
@@ -5,23 +5,23 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-Category.belongsTo(Product, {
+Product.belongsTo(Category, {
   foreignKey: 'category_id'
 })
 // Categories have many Products
-Product.hasMany(Category, {
+Category.hasMany(Product, {
   foreignKey: 'category_id'
 })
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
   through:ProductTag,
-  as: product_tag,
+
   foreignKey: 'product_id'
 })
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
   through:ProductTag,
-  as: product_tag,
+
   foreignKey: 'tag_id'
 })
 module.exports = {
